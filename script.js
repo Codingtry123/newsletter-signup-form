5b6c9daae2bff7ab70dd243c41ae65fa611878aa
Mey Zhang <meyzhang@MeydeMacBook-Air.local>
2024-09-01 02:03:46 +1000

    update

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let img = document.getElementsByClassName('img')[0];
 let main = document.getElementsByClassName('main')[0];
 let input = document.getElementsByTagName('input')[0];
 let success = document.getElementsByClassName('success')[0];
+let h1 = document.getElementsByClassName('h1')[0];
 function validateEmail(email) {
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     return emailPattern.test(email);
@@ -14,7 +15,8 @@ event.preventDefault();
     if(validateEmail(input.value)){
         main.classList.remove('error');
     main.classList.toggle('hidden');
-    success.classList.toggle('hidden');}
+    success.classList.toggle('hidden');
+    h1.textContent = `A confirmation email has been sent to ${input.value}.Please open it and click the button inside to confirm your subscription.`}
     else{
         main.classList.add('error');
     }
